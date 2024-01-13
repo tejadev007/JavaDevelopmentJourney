0b0b59090c1dd80812098147b22281e54da08242
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-13 20:14:19 +0000

    b shape by class

--- src/learning/bShapeStars.java
+++ src/learning/bShapeStars.java
@@ -16,6 +16,16 @@ public class bShapeStars {
                 else {
                     System.out.print(" ");
                 }
+                
+            }
+            
+            for(int j=0; j<=n; j++){
+                if(j==0 || j== (n)/2 && i!=0 && i!= (n)/2 && i!=(n) || i==(n)/2 && j<(n)/2 || i==(n) && j<(n)/2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
