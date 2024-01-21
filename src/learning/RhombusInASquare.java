95c68168d50fe1149fc2a65eef98a08fbabddb2c
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-21 05:49:02 -0500

    learning Date: 1/21/2024 Commit 2

---
+++ src/learning/RhombusInASquare.java
@@ -0,0 +1,21 @@
+package learning;
+
+public class RhombusInASquare {
+    public static void main(String args[]){
+        int n = 20;
+        for(int i = 0; i <= n-1; i++){
+            for(int j = 0; j <= n-1; j++){
+                if(i==0 || j==0 || i==n-1 || j == n-1 || i+j == (n-1)/2 /*P1*/
+                    || i-j == (n-1)/2 /*P2*/
+                    || j-i == (n-1)/2 /*P3*/
+                    || i+j == n+(n-1)/2 /*P4*/){ //it is not exact. but, it is approximate
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
