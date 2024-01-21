dbc6b6688d7b11f45ebeac20e95108bbdcc20f96
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-21 06:49:25 -0500

    learning Date: 1/21/2024 Commit 4 (K star Program)

---
+++ src/learning/KShapedStar.java
@@ -0,0 +1,18 @@
+package learning;
+// This is the loops program using the * to make a K in console
+public class KShapedStar {
+    public static void main(String [] args){
+        int n = 25;
+        for(int i = 0; i <= n-1; i++){
+            for(int j = 0; j <= n-1; j++){
+                if(j==0 || i+j == (n-1)/2 || i-j == (n-1)/2){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
