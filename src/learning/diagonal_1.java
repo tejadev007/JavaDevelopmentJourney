d3d6afeee72c8eb3a65061ed36f4836324394041
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-21 05:58:11 -0500

    learning Date: 1/21/2024 Commit 3 (Learning quote: public static void main(String []args) correct way to do is like this!

--- src/learning/diagonal_1.java
+++ src/learning/diagonal_1.java
@@ -1,7 +1,7 @@
 package learning;
 
 public class diagonal_1 {
-    public static void main(String args[]){
+    public static void main(String []arg){
         int n = 20;
         for(int i = 0; i <= n; i++){
             for (int j = 0; j <= n; j++){
--- src/learning/diagonal_2.java
+++ src/learning/diagonal_2.java
@@ -1,7 +1,7 @@
 package learning;
 
 public class diagonal_2 {
-    public static void main(String args[]){
+    public static void main(String []args){
         int n = 20;
         for(int i = 0; i <= n-1; i++){
             for(int j = 0; j <= n-1; j++){
