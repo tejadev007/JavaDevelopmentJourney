d38e5c1c76a4231e56886e19aa181a9d417309db
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-22 18:24:53 -0500

    learning Date: 1/22/2024 Commit 1 Z shaped stars!

--- src/practice/myName.java
+++ src/practice/myName.java
@@ -1,7 +1,7 @@
 package practice;
 
 public class myName {
-    public static void main(String args[]){
+    public static void main(String []args){
         int n = 15;
         for(int i = 0; i<= n; i++){
             for(int j = 0; j<=n; j++){
