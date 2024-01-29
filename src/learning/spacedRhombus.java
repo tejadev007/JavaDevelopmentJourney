e30ba0c8921560050c82d7dd71cf40ec78381432
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-28 22:27:27 -0500

    learning Date: 1/28/2024 Commit 1
    Rhombus Code Complete!

---
+++ src/learning/spacedRhombus.java
@@ -0,0 +1,18 @@
+package learning;
+
+public class spacedRhombus {
+    public static void main(String[]args){
+        int n = 20;
+        for(int i=0; i<=n-1; i++){
+            for(int j=0; j<=n-1; j++){
+                if(i+j <= (n-1)/2 || i-j >=(n-1)/2 || j-i >= (n-1)/2 || i+j >= n+(n-1)/2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
