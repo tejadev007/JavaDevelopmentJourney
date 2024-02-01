3d1f04bd90bafa33c20f25f12bf7254cad6909df
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-01-31 21:10:17 -0500

    learning Date: 1/31/2024 Commit 3
    switch case program practice done!

---
+++ src/practice/SwitchCase.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class SwitchCase {
+    public static void main(String[] args){
+        int j = 30;
+        switch(j){
+            case 1:{
+                System.out.println("case 1 executed");
+                break;
+            }
+            case 20:{
+                System.out.println("case 20 executed");
+                break;
+            }
+            case 30:{
+                System.out.println("case 30 executed");
+                break;
+            }
+            case 40:{
+                System.out.println("case 40 executed");
+                break;
+            }
+            default:{
+                System.out.println("case default is executed");
+            }
+        }
+    }
+}
