0faf40fd98c03fdcf5577ce14a04e627ca2237c4
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-02-08 21:42:13 +0000

    Creation of object (Basic)

---
+++ src/ObjectOrientedProgramming/student.java
@@ -0,0 +1,11 @@
+class student{
+        String name = "Snoopy";
+        void show(){
+            System.out.println("Name of the dog is "+name);
+        }
+    public static void main(String[] args){
+        student st = new student(); //Creation of object & object type is the class name.
+        st.show();
+        
+    }
+}
