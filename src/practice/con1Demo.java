6772edd9414c06d52a85d661745ca6b566db9a7e
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-06-11 12:15:55 -0400

    Morning practice 6/11/2024 constructor using getters and setters Commit count: 1

---
+++ src/practice/con1Demo.java
@@ -0,0 +1,42 @@
+package practice;
+
+class Dog {
+    private String name;
+    private int age;
+    private int cost;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
+public class con1Demo {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.setName("TeJa");
+        d1.setAge(20);
+        d1.setCost(12000);
+        System.out.println(d1.getName());
+        System.out.println(d1.getAge());
+        System.out.println(d1.getCost());
+    }
+}
