25a28e356638a7d0c1665247a22a7b252e235aa9
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-09-24 23:59:24 -0400

    Tuesday 9/24/2024 Commit number 2 ConDemo7

---
+++ src/learning/ConstructorEncapsulation/learning/ConstructorEncapsulation/Con7Demo.java
@@ -0,0 +1,16 @@
+package learning.ConstructorEncapsulation.learning.ConstructorEncapsulation;
+
+public class Con7Demo {
+    static int age;
+
+    static
+    {
+        age = 10;
+        System.out.println("Age: "+age);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
