89ddce59317741703d48db68a258ab10c2c043c3
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2024-10-15 18:20:12 -0500

    Tuesday 10/15/2024 Commit number 2 Topic inheritance files: Demo2

---
+++ src/learning/inheritance/demo2.java
@@ -0,0 +1,24 @@
+package learning.inheritance;
+
+class A{
+    void run(){
+        System.out.println("A");
+    }
+}
+
+class B {
+    void run(){
+        System.out.println("B");
+    }
+}
+
+// If here i want extend both the classes into a new c class
+// then it wont work because it will becomne multiple inheritance and create an ambiguity
+// Ambiguity here is that if the demo 2 class extends the class c and if we create a object and wanna
+// call run method, complier will get confused because of the same method present in both the classes
+//Thus the ambiguity is created
+public class demo2 {
+    public static void main(String[] args) {
+
+    }
+}
