906ab13f64885890bd822c1ab528a36f4fbbbcf9
tejadev007 <92089207+tejadev007@users.noreply.github.com>
2025-03-19 13:59:40 -0500

    practice

--- src/practice/diagonal1.java
+++ src/practice/diagonal1.java
@@ -1,4 +1,18 @@
 package practice;
 
 public class diagonal1 {
+    public static void main(String[] args) {
+        int n = 20;
+        for (int i = 0; i<=n; i++){
+            for (int j = 0; j <= n; j++){
+                if(i==j){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- src/practice/diagonal2.java
+++ src/practice/diagonal2.java
@@ -1,4 +1,17 @@
 package practice;
 
 public class diagonal2 {
+    public static void main(String[] args) {
+        int n = 25;
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n; j++){
+                if(i+j == n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
